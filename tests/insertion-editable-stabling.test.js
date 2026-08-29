5692f46ea078728863516d6ad534d067b2c0387e
Nazif Jaafar <nazif92@gmail.com>
2026-08-29 22:14:44 +0300

    Disable Refresh when insertion stabling matches (#411)

--- tests/insertion-editable-stabling.test.js
+++ tests/insertion-editable-stabling.test.js
@@ -43,6 +43,7 @@ test("Refresh shows an animated fire warning when editable positions differ from
   assert.match(pageSource, /function insertionStablingTrainPositionsMatch\([\s\S]*normalizeTrainId/);
   assert.match(controlsSource, /isDirty \? "is-dirty" : "is-synced"/);
   assert.match(controlsSource, /<Flame className="theme-insertion-refresh-fire/);
+  assert.match(controlsSource, /onClick=\{onRefresh\}[\s\S]*disabled=\{!isDirty\}[\s\S]*disabled:cursor-not-allowed/);
   assert.match(stylesheetSource, /\.theme-insertion-refresh-button\.is-synced \{[\s\S]*background: #071828;[\s\S]*color: #7890a4;/);
   assert.match(pageSource, /westInsertionStablingDirty = !insertionStablingTrainPositionsMatch/);
   assert.match(pageSource, /eastInsertionStablingDirty = !insertionStablingTrainPositionsMatch/);
